95e7468ec2b5b3b455d5f262ed297067cae3157a
miketleach <miketleach@hotmail.com>
2016-12-10 20:30:25 +0000

    Changed test so it doesn’t need a copy of everything. Moved to v2test so robots.txt already hides it.

--- test/js/movesidebar.js
+++
@@ -1,9 +0,0 @@
-
-$( document ).ready(function() {
-    $(window).scroll(function(){
-    	//This is a slighty dodgy way of not applying animation to sidebar when we're in mobile view
-    	if($('#nav').css('display')!='none') {
-    		$("#sidebar1").stop().animate({"marginTop": ($(window).scrollTop()) + "px", "marginLeft":($(window).scrollLeft()) + "px"}, "fast" );
-		}
-	});
-});
